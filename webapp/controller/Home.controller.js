fa6190259c172bd2008dc1e5c1217d4ce6a8ef04
Hana2212 <nthaiyen0606@gmail.com>
2025-03-27 18:06:03 +0700

    DropdownList

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,10 +1,27 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("fioriv3.controller.Home", {
-        onInit() {
+        onInit: function () {
+            // Any initialization logic
+        },
+
+        onSetDataForDropDown: function (oEvent) {
+            
+            var oSelectedKey = this.byId("dropDownBukrs").getSelectedKey();
+            var oFilter = new Filter("Bukrs", FilterOperator.EQ, oSelectedKey);
+            var oSmartTable = this.byId("smartTable");
+        
+            // Push filter
+            var oBinding = oSmartTable.getTable().getBinding("rows") || oSmartTable.getTable().getBinding("items");
+            if (oBinding) {
+                oBinding.filter([oFilter]);
+            }
+    
         }
     });
 });
